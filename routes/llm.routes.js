e351183589dab46de1fee355349dacd18a2a09aa
Owen Roth <69156111+oroth8@users.noreply.github.com>
2024-02-27 20:56:46 -0600

    change get to post

--- routes/llm.routes.js
+++ routes/llm.routes.js
@@ -10,6 +10,6 @@ const router = express.Router();
 // Define routes with controllers and middlewares
 router.post("/add-schema", addSchema);
 router.get("/get-pr-data", getPrData);
-router.get("/add-pr-data", addPrData);
+router.post("/add-pr-data", addPrData);
 
 export default router;
